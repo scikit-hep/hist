dc9b209dfa5d2fa934a803c9cb589b414314fc13
N!no <ninomyemail@gmail.com>
2020-06-01 11:54:09 +0800

    NamedHist Init (#13)
    * NamedHist init
    
    * black reformat
    
    * allow Hist axes not to have names
    
    * ToDo: forbid non-named NamedHist
    
    * Update named.py
    
    * better-styled derive
    
    * forbid no-named/duplic axes for NamedHist
    
    * force names everywhere
    
    * add mypy to pre-commit
    
    * Update NamedHist.ipynb
    
    * Update dev-environment.yml
    
    * add tests of NamedHist
    
    * Update setup.cfg
    
    * Update dev-environment.yml
    
    * Update mypy CI check in #15
    
    * rm py3.6 for annotations
    
    * solved https://github.com/scikit-hep/hist/pull/13#discussion_r430142737
    
    * typing hints added
    
    * add basic_tests for StrCategory
    
    * add py3.6
    
    * numpy to core.py
    
    * rm __future__.annotations
    
    * correct regular expression of valid Py identifier
    
    * add tests for exceptions

--- src/hist/named.py
+++ src/hist/named.py
@@ -1,5 +1,80 @@
 from .core import BaseHist
+import numpy as np
 
 
 class NamedHist(BaseHist):
-    pass
+    def __init__(self, *args, **kwargs):
+        """
+            Initialize NamedHist object. Axis params must contain the names.
+        """
+
+        super(BaseHist, self).__init__(*args, **kwargs)
+        self.names: dict = dict()
+        for ax in self.axes:
+            if not ax.name:
+                raise Exception(
+                    "Each axes in the NamedHist instance should have a name."
+                )
+            elif ax.name in self.names:
+                raise Exception(
+                    "NamedHist instance cannot contain axes with duplicated names."
+                )
+            else:
+                self.names[ax.name] = True
+
+    def fill(self, *args, **kwargs):
+        """
+            Insert data into the histogram using names and return a \
+            NamedHist object. NamedHist could only be filled by names.
+        """
+
+        indices = []
+        values = []
+        for name, val in kwargs.items():
+            for index, axis in enumerate(self.axes):
+                if name == axis.name:
+                    indices.append(index)
+                    values.append(val)
+                    break
+
+        d = dict(zip(indices, values))
+        l = sorted(d.items(), key=lambda item: item[0])
+        nd = np.asarray(l, dtype=object)
+        data = nd.ravel()[1::2]
+        if len(data) != len(self.axes):
+            raise Exception("The axis names could not be found when filling.")
+        super().fill(*data)
+
+        return self
+
+    def __getitem__(self, index):
+        """
+            Get histogram item.
+        """
+
+        if isinstance(index, dict):
+            k = list(index.keys())
+            if isinstance(k[0], str):
+                for key in k:
+                    for idx, axis in enumerate(self.axes):
+                        if key == axis.name:
+                            index[idx] = index.pop(key)
+                            break
+
+        return super().__getitem__(index)
+
+    def __setitem__(self, index, value):
+        """
+            Set histogram item.
+        """
+
+        if isinstance(index, dict):
+            k = list(index.keys())
+            if isinstance(k[0], str):
+                for key in k:
+                    for idx, axis in enumerate(self.axes):
+                        if key == axis.name:
+                            index[idx] = index.pop(key)
+                            break
+
+        return super().__setitem__(index, value)
--- tests/test_named.py
+++ tests/test_named.py
@@ -1,16 +1,411 @@
 from hist import axis, NamedHist
+import boost_histogram as bh
+import pytest
+import numpy as np
 
 
 def test_basic_usage():
+    """
+        Test basic usage -- whether NamedHist are properly derived from\
+        boost-histogram and whether it can be filled by names.
+    """
+
+    # Basic
+    h = NamedHist(axis.Regular(10, 0, 1, name="x")).fill(x=[0.35, 0.35, 0.45])
+
+    for idx in range(10):
+        if idx == 3:
+            assert h[idx] == h[{0: idx}] == h[{"x": idx}] == 2
+        elif idx == 4:
+            assert h[idx] == h[{0: idx}] == h[{"x": idx}] == 1
+        else:
+            assert h[idx] == h[{0: idx}] == h[{"x": idx}] == 0
+
+    # Regular
+    h = h = NamedHist(
+        axis.Regular(10, 0, 1, name="x"),
+        axis.Regular(10, 0, 1, name="y"),
+        axis.Regular(2, 0, 2, name="z"),
+    ).fill(
+        x=[0.35, 0.35, 0.35, 0.45, 0.55, 0.55, 0.55],
+        y=[0.35, 0.35, 0.45, 0.45, 0.45, 0.45, 0.45],
+        z=[0, 0, 1, 1, 1, 1, 1],
+    )
+
+    z_one_only = h[{"z": bh.loc(1)}]
+    for idx_x in range(0, 10):
+        for idx_y in range(0, 10):
+            if idx_x == 3 and idx_y == 4:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 1
+                )
+            elif idx_x == 4 and idx_y == 4:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 1
+                )
+            elif idx_x == 5 and idx_y == 4:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 3
+                )
+            else:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 0
+                )
+
+    # Bool
+    h = NamedHist(axis.Bool(name="x"), axis.Bool(name="y"), axis.Bool(name="z"),).fill(
+        x=[True, True, True, True, True, False, True],
+        y=[False, True, True, False, False, True, False],
+        z=[False, False, True, True, True, True, True],
+    )
+
+    z_one_only = h[{"z": bh.loc(True)}]
+    assert z_one_only[False, False] == z_one_only[{"x": False, "y": False}] == 0
+    assert z_one_only[False, True] == z_one_only[{"x": False, "y": True}] == 1
+    assert z_one_only[True, False] == z_one_only[{"x": True, "y": False}] == 3
+    assert z_one_only[True, True] == z_one_only[{"x": True, "y": True}] == 1
+
+    # Variable
     h = NamedHist(
-        axis.Regular(10, 0, 1, title="x")
-    )  # NamedHist should require axis.Regular to have a name set
+        axis.Variable(range(11), name="x"),
+        axis.Variable(range(11), name="y"),
+        axis.Variable(range(3), name="z"),
+    ).fill(
+        x=[3.5, 3.5, 3.5, 4.5, 5.5, 5.5, 5.5],
+        y=[3.5, 3.5, 4.5, 4.5, 4.5, 4.5, 4.5],
+        z=[0, 0, 1, 1, 1, 1, 1],
+    )
 
-    h.fill([0.35, 0.35, 0.45])  # Fill should be keyword only, with the names
+    z_one_only = h[{"z": bh.loc(1)}]
+    for idx_x in range(0, 10):
+        for idx_y in range(0, 10):
+            if idx_x == 3 and idx_y == 4:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 1
+                )
+            elif idx_x == 4 and idx_y == 4:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 1
+                )
+            elif idx_x == 5 and idx_y == 4:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 3
+                )
+            else:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 0
+                )
 
-    assert h[2] == 0
-    assert h[3] == 2
-    assert h[4] == 1
-    assert h[5] == 0
+    # Integer
+    h = NamedHist(
+        axis.Integer(0, 10, name="x"),
+        axis.Integer(0, 10, name="y"),
+        axis.Integer(0, 2, name="z"),
+    ).fill(
+        x=[3.5, 3.5, 3.5, 4.5, 5.5, 5.5, 5.5],
+        y=[3.5, 3.5, 4.5, 4.5, 4.5, 4.5, 4.5],
+        z=[0, 0, 1, 1, 1, 1, 1],
+    )
 
-    # assert h[{'x':2}] # Should work
+    z_one_only = h[{"z": bh.loc(1)}]
+    for idx_x in range(0, 10):
+        for idx_y in range(0, 10):
+            if idx_x == 3 and idx_y == 4:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 1
+                )
+            elif idx_x == 4 and idx_y == 4:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 1
+                )
+            elif idx_x == 5 and idx_y == 4:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 3
+                )
+            else:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 0
+                )
+
+    # IntCategory
+    h = NamedHist(
+        axis.IntCategory(range(10), name="x"),
+        axis.IntCategory(range(10), name="y"),
+        axis.IntCategory(range(2), name="z"),
+    ).fill(
+        x=[3.5, 3.5, 3.5, 4.5, 5.5, 5.5, 5.5],
+        y=[3.5, 3.5, 4.5, 4.5, 4.5, 4.5, 4.5],
+        z=[0, 0, 1, 1, 1, 1, 1],
+    )
+
+    z_one_only = h[{"z": bh.loc(1)}]
+    for idx_x in range(0, 10):
+        for idx_y in range(0, 10):
+            if idx_x == 3 and idx_y == 4:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 1
+                )
+            elif idx_x == 4 and idx_y == 4:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 1
+                )
+            elif idx_x == 5 and idx_y == 4:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 3
+                )
+            else:
+                assert (
+                    z_one_only[idx_x, idx_y]
+                    == z_one_only[{"x": idx_x, "y": idx_y}]
+                    == 0
+                )
+
+    # StrCategory
+    h = NamedHist(
+        axis.StrCategory("FT", name="x"),
+        axis.StrCategory(list("FT"), name="y"),
+        axis.StrCategory(["F", "T"], name="z"),
+    ).fill(
+        x=["T", "T", "T", "T", "T", "F", "T"],
+        y=["F", "T", "T", "F", "F", "T", "F"],
+        z=["F", "F", "T", "T", "T", "T", "T"],
+    )
+
+    z_one_only = h[{"z": bh.loc("T")}]
+    assert z_one_only[bh.loc("F"), bh.loc("F")] == 0
+    assert z_one_only[bh.loc("F"), bh.loc("T")] == 1
+    assert z_one_only[bh.loc("T"), bh.loc("F")] == 3
+    assert z_one_only[bh.loc("T"), bh.loc("T")] == 1
+
+
+def test_errors():
+
+    # right axis names
+    assert NamedHist(axis.Regular(50, -3, 3, name="x0"))
+    assert NamedHist(axis.Bool(name="x_"))
+    assert NamedHist(axis.Variable(range(-3, 3), name="xx"))
+    assert NamedHist(axis.Integer(-3, 3, name="x_x"))
+    assert NamedHist(axis.IntCategory(range(-3, 3), name="X__X"))
+    assert NamedHist(axis.StrCategory("FT", name="X00"))
+
+    # wrong axis names: protected or private prefix
+    with pytest.raises(Exception):
+        NamedHist(axis.Regular(50, -3, 3, name="_x"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Bool(name="__x"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Variable(range(-3, 3), name="_x_"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Integer(-3, 3, name="_x0"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.IntCategory(range(-3, 3), name="_0x"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.StrCategory("FT", name="_xX"))
+
+    # wrong axis names: number prefix
+    with pytest.raises(Exception):
+        NamedHist(axis.Regular(50, -3, 3, name="0"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Bool(name="00"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Variable(range(-3, 3), name="0x"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Integer(-3, 3, name="0_x"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.IntCategory(range(-3, 3), name="00x"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.StrCategory("FT", name="0xx"))
+
+    # wrong axis names: unsupported chr
+    with pytest.raises(Exception):
+        NamedHist(axis.Regular(50, -3, 3, name="-x"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Bool(name="x-"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Variable(range(-3, 3), name="?x"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Integer(-3, 3, name="%x"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.IntCategory(range(-3, 3), name="#x"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.StrCategory("FT", name="*x"))
+
+    # wrong axis names: empty names
+    with pytest.raises(Exception):
+        NamedHist(axis.Regular(50, -3, 3, name=""))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Bool(name=""))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Variable(range(-3, 3)))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Integer(-3, 3, name=""))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.IntCategory(range(-3, 3), name=""))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.StrCategory("FT"))
+
+    # right histogram axis names
+    assert NamedHist(
+        axis.Regular(50, -3, 3, name="x"), axis.Regular(50, -3, 3, name="y")
+    ).fill(x=np.random.randn(10), y=np.random.randn(10))
+
+    assert NamedHist(axis.Bool(name="x"), axis.Bool(name="y")).fill(
+        y=[True, False, True], x=[True, False, True]
+    )
+
+    assert NamedHist(
+        axis.Variable(range(-3, 3), name="x"), axis.Variable(range(-3, 3), name="y")
+    ).fill(x=np.random.randn(10), y=np.random.randn(10))
+
+    assert NamedHist(axis.Integer(-3, 3, name="x"), axis.Integer(-3, 3, name="y")).fill(
+        x=np.random.randn(10), y=np.random.randn(10)
+    )
+
+    assert NamedHist(
+        axis.IntCategory(range(-3, 3), name="x"),
+        axis.IntCategory(range(-3, 3), name="y"),
+    ).fill(x=np.random.randn(10), y=np.random.randn(10))
+
+    assert NamedHist(
+        axis.StrCategory(["F", "T"], name="x"), axis.StrCategory("FT", name="y")
+    ).fill(y=["T", "F", "T"], x=["T", "F", "T"])
+
+    # wrong histogram axis names: with the same names
+    with pytest.raises(Exception):
+        NamedHist(axis.Regular(50, -3, 3, name="x"), axis.Regular(50, -3, 3, name="x"))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Bool(name="y"), axis.Bool(name="y"))
+
+    with pytest.raises(Exception):
+        NamedHist(
+            axis.Variable(range(-3, 3), name="x"), axis.Variable(range(-3, 3), name="x")
+        )
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Integer(-3, 3, name="x"), axis.Integer(-3, 3, name="x"))
+
+    with pytest.raises(Exception):
+        NamedHist(
+            axis.IntCategory(range(-3, 3), name="x"),
+            axis.IntCategory(range(-3, 3), name="x"),
+        )
+
+    with pytest.raises(Exception):
+        NamedHist(
+            axis.StrCategory("TF", name="y"), axis.StrCategory(["T", "F"], name="y")
+        )
+
+    # wrong histogram axis names: fill without names
+    with pytest.raises(Exception):
+        NamedHist(
+            axis.Regular(50, -3, 3, name="x"), axis.Regular(50, -3, 3, name="y")
+        ).fill(np.random.randn(10), np.random.randn(10))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Bool(name="x"), axis.Bool(name="y")).fill(
+            [True, False, True], [True, False, True]
+        )
+
+    with pytest.raises(Exception):
+        NamedHist(
+            axis.Variable(range(-3, 3), name="x"), axis.Variable(range(-3, 3), name="y")
+        ).fill(np.random.randn(10), np.random.randn(10))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Integer(-3, 3, name="x"), axis.Integer(-3, 3, name="y")).fill(
+            np.random.randn(10), np.random.randn(10)
+        )
+
+    with pytest.raises(Exception):
+        NamedHist(
+            axis.IntCategory(range(-3, 3), name="x"),
+            axis.IntCategory(range(-3, 3), name="y"),
+        ).fill(np.random.randn(10), np.random.randn(10))
+
+    with pytest.raises(Exception):
+        NamedHist(
+            axis.StrCategory(["F", "T"], name="x"), axis.StrCategory("FT", name="y")
+        ).fill(["T", "F", "T"], ["T", "F", "T"])
+
+    # wrong histogram axis names: fill by wrong names
+    with pytest.raises(Exception):
+        NamedHist(
+            axis.Regular(50, -3, 3, name="x"), axis.Regular(50, -3, 3, name="y")
+        ).fill(x=np.random.randn(10), z=np.random.randn(10))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Bool(name="x"), axis.Bool(name="y")).fill(
+            y=[True, False, True], z=[True, False, True]
+        )
+
+    with pytest.raises(Exception):
+        NamedHist(
+            axis.Variable(range(-3, 3), name="x"), axis.Variable(range(-3, 3), name="y")
+        ).fill(z=np.random.randn(10), x=np.random.randn(10))
+
+    with pytest.raises(Exception):
+        NamedHist(axis.Integer(-3, 3, name="x"), axis.Integer(-3, 3, name="y")).fill(
+            x=np.random.randn(10), z=np.random.randn(10)
+        )
+
+    with pytest.raises(Exception):
+        NamedHist(
+            axis.IntCategory(range(-3, 3), name="x"),
+            axis.IntCategory(range(-3, 3), name="y"),
+        ).fill(y=np.random.randn(10), z=np.random.randn(10))
+
+    with pytest.raises(Exception):
+        NamedHist(
+            axis.StrCategory(["F", "T"], name="x"), axis.StrCategory("FT", name="y")
+        ).fill(z=["T", "F", "T"], x=["T", "F", "T"])
