b46eee351d6eedd97cac534d806ec60b6e623ec1
LovelyBuggies <ninomyemail@gmail.com>
2020-06-17 12:56:35 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Hist'
-copyright = '2020, Scikit-HEP'
-author = 'Henry Schreiner and Nino Lau'
+project = "Hist"
+copyright = "2020, Scikit-HEP"
+author = "Henry Schreiner and Nino Lau"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,19 +28,19 @@ author = 'Henry Schreiner and Nino Lau'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	'sphinx.ext.autodoc',
-    'nbsphinx',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon'
+    "sphinx.ext.autodoc",
+    "nbsphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', '**.ipynb_checkpoints', 'Thumbs.db', '.DS_Store', '.env']
+exclude_patterns = ["_build", "**.ipynb_checkpoints", "Thumbs.db", ".DS_Store", ".env"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -48,9 +48,9 @@ exclude_patterns = ['_build', '**.ipynb_checkpoints', 'Thumbs.db', '.DS_Store', 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
