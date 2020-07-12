5dd1b4faef1421e165f86bb6c30baccc78bc0cd0
N!no <ninomyemail@gmail.com>
2020-07-12 16:11:10 +0800

    test: complex index (#52)
    * test: complex index
    
    * refactor: move
    
    * refactor: based on master

--- tests/test_base.py
+++ tests/test_base.py
@@ -11,9 +11,9 @@ def test_version():
     assert hist.__version__ is not None
 
 
-def test_core_init():
+def test_base_init():
     """
-        Test core init -- whether BaseHist can be properly initialized.
+        Test base init -- whether BaseHist can be properly initialized.
     """
 
     # basic
@@ -100,9 +100,9 @@ def test_core_init():
         )
 
 
-def test_core_fill():
+def test_base_fill():
     """
-        Test core fill -- whether BaseHist can be properly filled.
+        Test base fill -- whether BaseHist can be properly filled.
     """
 
     # Regular
@@ -272,9 +272,9 @@ def test_core_fill():
     ).fill(np.random.normal(size=10))
 
 
-def test_core_access():
+def test_base_access():
     """
-        Test core access -- whether BaseHist bins can be accessed.
+        Test base access -- whether BaseHist bins can be accessed.
     """
 
     h = BaseHist(axis.Regular(10, -5, 5, name="X", title="x [units]")).fill(
@@ -301,9 +301,9 @@ def test_core_access():
     assert h[0j, -0j + 2, "hi", True, 1]
 
 
-def test_core_project():
+def test_base_project():
     """
-        Test core project -- whether BaseHist can be projected properly.
+        Test base project -- whether BaseHist can be projected properly.
     """
 
     h = BaseHist(
@@ -360,9 +360,9 @@ def test_core_project():
         h.project("G", "H")
 
 
-def test_core_plot1d():
+def test_base_plot1d():
     """
-        Test core plot1d -- whether 1d-BaseHist can be plotted properly.
+        Test base plot1d -- whether 1d-BaseHist can be plotted properly.
     """
 
     h = BaseHist(
@@ -395,9 +395,9 @@ def test_core_plot1d():
         h.project("B").plot1d(ls="red")
 
 
-def test_core_plot2d():
+def test_base_plot2d():
     """
-        Test core plot2d -- whether 2d-BaseHist can be plotted properly.
+        Test base plot2d -- whether 2d-BaseHist can be plotted properly.
     """
 
     h = BaseHist(
@@ -433,9 +433,9 @@ def test_core_plot2d():
         h.plot2d(cmap=0.1)
 
 
-def test_core_plot2d_full():
+def test_base_plot2d_full():
     """
-        Test core plot2d_full -- whether 2d-BaseHist can be fully plotted properly.
+        Test base plot2d_full -- whether 2d-BaseHist can be fully plotted properly.
     """
 
     h = BaseHist(
@@ -482,9 +482,9 @@ def test_core_plot2d_full():
         h.plot2d_full(main_cmap=0.1, side_lw="autumn")
 
 
-def test_core_plot():
+def test_base_plot():
     """
-        Test core plot -- whether BaseHist can be plotted properly.
+        Test base plot -- whether BaseHist can be plotted properly.
     """
 
     h = BaseHist(
@@ -539,9 +539,9 @@ def test_core_plot():
         h.project("A", "C").plot(cmap=0.1)
 
 
-def test_core_plot_pull():
+def test_base_plot_pull():
     """
-        Test core plot_pull -- whether 1d-BaseHist can be plotted pull properly.
+        Test base plot_pull -- whether 1d-BaseHist can be plotted pull properly.
     """
 
     h = BaseHist(
@@ -646,3 +646,49 @@ def test_core_plot_pull():
     # wrong kwargs types
     with pytest.raises(Exception):
         h.plot_pull(pdf, eb_ecolor=1.0, eb_mfc=1.0)  # kwargs should be str
+
+
+def test_base_index_access():
+    """
+        Test base index access -- whether BaseHist can be accessed by index.
+    """
+
+    h = BaseHist(
+        axis.Regular(10, -5, 5, name="Norm", title="normal distribution"),
+        axis.Regular(10, -5, 5, name="Unif", title="uniform distribution"),
+        axis.StrCategory(["hi", "hello"], name="Greet"),
+        axis.Boolean(name="Yes"),
+        axis.Integer(0, 10, name="Int"),
+    ).fill(
+        np.random.normal(size=10),
+        np.random.uniform(size=10),
+        ["hi"] * 8 + ["hello"] * 2,
+        [True] * 6 + [False] * 4,
+        np.ones(10),
+    )
+
+    assert (
+        h[1j, 1j, "hi", True, 1]
+        == h[6, 6, bh.loc("hi"), bh.loc(True), bh.loc(1)]
+        == h[0j + 1, -2j + 3, "hi", True, 1]
+        == h[bh.loc(1, 0), bh.loc(2, -1), "hi", True, 1]
+    )
+
+    assert h[0:10:2j, 0:5:5j, "hello", False, 5]
+
+    # wrong loc shortcut
+    with pytest.raises(Exception):
+        h[0.5, 1 / 2, "hi", True, 1]
+
+    with pytest.raises(Exception):
+        h[0.5 + 1j, 1 / 2 + 1j, "hi", True, 1]
+
+    # wrong rebin shortcut
+    with pytest.raises(Exception):
+        h[0:10:0.2j, 0:5:0.5j, "hello", False, 5]
+
+    with pytest.raises(Exception):
+        h[0 : 10 : 1 + 2j, 0 : 5 : 1 + 5j, "hello", False, 5]
+
+    with pytest.raises(Exception):
+        h[0:10:20j, 0:5:10j, "hello", False, 5]
