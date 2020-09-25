d2ac7dc6115503074f545f477372ac32f07f0d87
Henry Schreiner <henryschreineriii@gmail.com>
2020-09-24 23:05:22 -0400

    feat: new boost-histogram

--- tests/test_bh.py
+++ tests/test_bh.py
@@ -5,14 +5,16 @@ import hist
 
 def test_bh_conversion():
     h = bh.Histogram(bh.axis.Regular(3, 2, 1, metadata={"name": "x"}))
+    h.axes[0].name = "y"
 
     h2 = hist.Hist(h)
 
     assert isinstance(h2.axes[0], hist.axis.Regular)
-    assert h2.axes[0].name == "x"
-    assert h2.axes[0].metadata == {"name": "x"}  # MAY CHANGE
+    assert h2.axes[0].name == "y"
+    assert h2.axes[0].metadata == {"name": "x"}
 
     h3 = bh.Histogram(h2)
 
     assert not isinstance(h3.axes[0], hist.axis.Regular)
+    assert h2.axes[0].name == "y"
     assert h3.axes[0].metadata == {"name": "x"}
