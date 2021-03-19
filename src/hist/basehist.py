fix: sign issue

src/hist/basehist.py
@@ -310,7 +310,7 @@ class BaseHist(bh.Histogram, metaclass=MetaConstructor, family=hist):
         den_err = np.sqrt(np.sum(variances, axis=axis))
 
         new_values = num / den
-        new_variances = (num_err / den) ** 2 + (den_err * num / den ** 2) ** 2
+        new_variances = (num_err / den) ** 2 - (den_err * num / den ** 2) ** 2
 
         retval = self.__class__(*axes, storage=hist.storage.Mean())
         retval[...] = np.stack([count, new_values, new_variances], axis=-1)