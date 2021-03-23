e12f7485f823931ab1298a0ce2ee55f78b0add4d
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2021-03-23 16:49:28 -0400

    fix(types): better return type (#166)
    * fix(types): better return type
    
    * fix(typing): drop extra import

--- src/hist/plot.py
+++ src/hist/plot.py
@@ -6,8 +6,6 @@ import numpy as np
 
 import hist
 
-from .typing import ArrayLike
-
 try:
     import matplotlib.axes
     import matplotlib.patches as patches
@@ -82,7 +80,7 @@ def _curve_fit_wrapper(
     ydata: np.ndarray,
     yerr: np.ndarray,
     likelihood: bool = False,
-) -> Tuple[Tuple[float, ...], ArrayLike]:
+) -> Tuple[Tuple[float, ...], np.ndarray]:
     """
     Wrapper around `scipy.optimize.curve_fit`. Initial parameters (`p0`)
     can be set in the function definition with defaults for kwargs
