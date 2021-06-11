cad3c6a9ab3b33f04230c6b2e869c36085009cd1
Aman Goel <aman.goel185@gmail.com>
2021-06-11 17:03:49 +0530

    docs: add MyST parser extension

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx_copybutton",
+    "myst_parser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
