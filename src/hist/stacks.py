1a3dd37074e4b4ec858cece9a67169b60496f9f3
N!no <ninomyemail@gmail.com>
2021-07-03 10:19:53 -0400

    feat: allow stack(ax1, ax2) but not allow plot, and without tests

--- src/hist/stacks.py
+++ src/hist/stacks.py
@@ -1,6 +1,9 @@
 import sys
 from typing import Any, Optional
 
+from .axis import AxesMixin
+from .basehist import BaseHist
+
 try:
     from mplhep.plot import histplot
 except ModuleNotFoundError:
@@ -19,25 +22,38 @@ class Stack:
         """
         Initialize Stack for histograms and axes.
         """
-        # todo: make the args support axes.
-
         if len(args) == 0:
+            raise ValueError("There should be histograms or axes in stack")
+
+        if all([isinstance(a, BaseHist) for a in args]):
+            axes_type = [type(ax) for ax in args[0].axes]
+            for a in args:
+                if axes_type != [type(ax) for ax in a.axes]:
+                    raise ValueError("Histograms' axes don't match")
+
+            self._stack = [*args]
+            self._stack_len = len(args)
+
+        elif all([isinstance(a, AxesMixin) for a in args]):
+            if len({type(ax) for ax in args}) != 1:
+                raise ValueError("Axes don't match")
+
+            self._stack = [*args]
+            self._stack_len = len(args)
+
+        else:
             raise ValueError("There should be histograms or axes in Stack")
-
-        axes_type = [type(ax) for ax in args[0].axes]
-        for h in args:
-            if axes_type != [type(ax) for ax in h.axes]:
-                raise ValueError("Histograms' axes don't match")
-
-        self._stack = [*args]
-        self._stack_len = len(args)
 
     def plot(self, *args: Any, overlay: "Optional[str]" = None, **kwargs: Any) -> "Any":
         """
         Plot method for Stack object.
         """
 
-        if self._stack[0].ndim == 1:
-            return histplot(self._stack, stack=True)
+        if all([isinstance(a, BaseHist) for a in args]):
+            if self._stack[0].ndim == 1:
+                return histplot(self._stack, stack=True)
+            else:
+                raise NotImplementedError("Please project to 1D before calling plot")
+
         else:
-            raise NotImplementedError("Please project to 1D before calling plot")
+            raise NotImplementedError("Only plot histogram stack are supported")
