957a0ed21488659d2ce21c634ec178bc9634c104
Aman Goel <aman.goel185@gmail.com>
2021-07-06 19:03:09 +0530

    fix: modify warning to display disallowed name

--- src/hist/basehist.py
+++ src/hist/basehist.py
@@ -96,11 +96,12 @@ class BaseHist(bh.Histogram, metaclass=MetaConstructor, family=hist):
             super().__init__(*args, storage=storage, metadata=metadata)  # type: ignore
 
             disallowed_names = ["weight", "sample", "threads"]
-            if any(item in disallowed_names for item in [ax.name for ax in self.axes]):
-                disallowed_warning = (
-                    f"{item} is a protected keyword and cannot be used as axis name"
-                )
-                warnings.warn(disallowed_warning)
+            for item in [ax.name for ax in self.axes]:
+                if item in disallowed_names:
+                    disallowed_warning = (
+                        f"{item} is a protected keyword and cannot be used as axis name"
+                    )
+                    warnings.warn(disallowed_warning)
 
             valid_names = [ax.name for ax in self.axes if ax.name]
             if len(valid_names) != len(set(valid_names)):
