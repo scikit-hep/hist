6aebba7feacbce15532065e483cea26810d1c587
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2022-07-04 09:28:22 -0400

    chore: move to hatchling (#418)
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- noxfile.py
+++ noxfile.py
@@ -29,7 +29,7 @@ def pylint(session: nox.Session) -> None:
     Run pylint.
     """
 
-    session.install("pylint")
+    session.install("pylint~=2.14.0")
     session.install("-e", ".")
     session.run("pylint", "src", *session.posargs)
 
