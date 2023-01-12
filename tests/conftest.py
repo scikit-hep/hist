c029217aab44f1d6c9ce935667c6affaafda5808
Lindsey Gray <lindsey.gray@gmail.com>
2023-01-12 15:03:07 -0600

    add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,11 @@ import pytest
 from hist import Hist, NamedHist
 from hist.hist import BaseHist
 
+try:
+    import hist.dask as dah
+except ImportError:
+    dah = None
+
 
 @pytest.fixture(params=[Hist, BaseHist, NamedHist])
 def named_hist(request):
@@ -13,4 +18,21 @@ def named_hist(request):
 
 @pytest.fixture(params=[Hist, BaseHist])
 def unnamed_hist(request):
+    yield request.param
+
+
+dask_params_named = []
+dask_params_unnamed = []
+if dah is not None:
+    dask_params_named = [dah.Hist, dah.NamedHist]
+    dask_params_unnamed = [dah.Hist]
+
+
+@pytest.fixture(params=dask_params_named)
+def named_dask_hist(request):
+    yield request.param
+
+
+@pytest.fixture(params=dask_params_unnamed)
+def unnamed_dask_hist(request):
     yield request.param
