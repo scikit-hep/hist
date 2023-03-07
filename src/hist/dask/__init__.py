2815fe2982b9d02305ed64329990b955c017471a
Henry Schreiner <henryschreineriii@gmail.com>
2023-03-06 22:54:14 -0500

    tests: use find_spec
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/hist/dask/__init__.py
+++ src/hist/dask/__init__.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
 
-try:
-    import dask_histogram
-except ModuleNotFoundError as err:
+import importlib.util
+
+if not importlib.util.find_spec("dask_histogram"):
     raise ModuleNotFoundError(
         """for hist.dask, install the 'dask_histogram' package with:
         pip install dask_histogram
         or
         conda install dask_histogram"""
-    ) from err
+    )
 
 from .hist import Hist
 from .namedhist import NamedHist
