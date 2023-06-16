efed7235df0bed6e7cba4ec1b146336642d90d08
Angus Hollands <goosey15@gmail.com>
2023-06-16 20:25:14 +0100

    feat: add `fill_flattened` support (#474)
    This PR draft is based upon
    https://gist.github.com/agoose77/f755d96061a36795dda592276d63f506, which
    prototyped what `fill_flattened` might look like in Hist. The purpose of
    this feature is to make histogramming with different-structure arrays
    more convenient.
    
    This implementation is fairy opinionated; it should be possible, for
    example to support pass in axis arrays by destructuring obejcts _and_
    keyword argument. However, in the pursuit of simplicity, I've made these
    mutually exclusive; either a single structured object should be used, or
    the `*args` and `**kwargs` are used as expected to map onto axes.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/hist/interop.py
@@ -0,0 +1,133 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+from typing import Any, Callable, Iterator, TypeVar, cast
+
+import numpy as np
+
+from ._compat.typing import ArrayLike, Protocol
+
+T_contra = TypeVar("T_contra", contravariant=True)
+U = TypeVar("U")
+V = TypeVar("V")
+
+
+class HistogramModuleProtocol(Protocol[T_contra, U]):
+    def unpack(self, obj: T_contra) -> dict[str, U] | None:
+        ...
+
+    def broadcast_and_flatten(
+        self, objects: Sequence[U | ArrayLike]
+    ) -> tuple[np.typing.NDArray[Any], ...]:
+        ...
+
+
+_histogram_modules: dict[type, HistogramModuleProtocol[Any, Any]] = {}
+
+
+def histogram_module_for(
+    cls: type,
+) -> Callable[[HistogramModuleProtocol[U, V]], HistogramModuleProtocol[U, V]]:
+    """
+    Register a histogram-module object for the given class.
+    """
+
+    def wrapper(obj: HistogramModuleProtocol[U, V]) -> HistogramModuleProtocol[U, V]:
+        _histogram_modules[cls] = obj
+        return obj
+
+    return wrapper
+
+
+def find_histogram_modules(
+    *objects: Any,
+) -> Iterator[HistogramModuleProtocol[Any, Any]]:
+    """
+    Yield histogram-module objects that are known to support any of the given objects.
+    """
+    for arg in objects:
+        try:
+            yield arg._histogram_module_
+        except AttributeError:
+            # Find class exactly, or check subclasses
+            for cls in type(arg).__mro__:
+                try:
+                    yield _histogram_modules[cls]
+                except KeyError:
+                    continue
+
+
+def destructure(obj: Any) -> dict[str, Any] | None:
+    """
+    Pull out named histogram-module arrays from the given structured object as a mapping.
+    The returned arrays should be compatible with `broadcast_and_flatten`.
+    If the argument is not understood as a structured object by any histogram modules,
+    return `None`.
+    """
+    for module in find_histogram_modules(obj):
+        return module.unpack(obj)
+    raise TypeError(f"No histogram module found for {obj!r}")
+
+
+def broadcast_and_flatten(args: Sequence[Any]) -> tuple[np.typing.NDArray[Any], ...]:
+    """
+    Convert the given histogram-module arrays into a set of consistent 1D NumPy arrays
+    for histogram filling. For NumPy this entails broadcasting and flattening.
+    """
+    for module in find_histogram_modules(*args):
+        result = module.broadcast_and_flatten(args)
+        if result is not NotImplemented:
+            return result
+
+    raise TypeError(f"No histogram module found for {args!r}")
+
+
+@histogram_module_for(np.ndarray)
+class NumpyHistogramModule:
+    @staticmethod
+    def unpack(obj: np.typing.NDArray[Any]) -> dict[str, np.typing.NDArray[Any]] | None:
+        if obj.dtype.fields is None:
+            return None
+
+        return {k: obj[k] for k in obj.dtype.fields}
+
+    @staticmethod
+    def broadcast_and_flatten(
+        args: Sequence[np.typing.NDArray[Any] | ArrayLike],
+    ) -> tuple[np.typing.NDArray[Any], ...]:
+        arrays = []
+        for arg in args:
+            # If we can't interpret this argument, it's not NumPy-friendly!
+            try:
+                arrays.append(np.asarray(arg))
+            except (TypeError, ValueError):
+                return NotImplemented
+
+        return tuple(np.ravel(x) for x in np.broadcast_arrays(*arrays))
+
+
+try:
+    import pandas as pd
+except ImportError:
+    ...
+else:
+
+    @histogram_module_for(pd.DataFrame)
+    class PandasHistogramModule:
+        @staticmethod
+        def unpack(obj: pd.DataFrame) -> dict[str, pd.Series[Any]]:
+            return cast(dict[str, pd.Series[Any]], obj.to_dict("series"))
+
+        @staticmethod
+        def broadcast_and_flatten(
+            args: Sequence[pd.Series[Any] | ArrayLike],
+        ) -> tuple[np.typing.NDArray[Any], ...]:
+            arrays = []
+            for arg in args:
+                # If we can't interpret this argument, it's not NumPy-friendly!
+                try:
+                    arrays.append(np.asarray(arg))
+                except (TypeError, ValueError):
+                    return NotImplemented
+
+            return tuple(np.ravel(x) for x in np.broadcast_arrays(*arrays))
--- src/hist/namedhist.py
+++ src/hist/namedhist.py
@@ -6,6 +6,7 @@ import boost_histogram as bh
 
 import hist
 
+from . import interop
 from ._compat.typing import ArrayLike, Self
 from .basehist import BaseHist, IndexingExpr
 
@@ -53,6 +54,68 @@ class NamedHist(BaseHist, family=hist):
 
         raise TypeError(
             f"Only fill by names are supported for {self.__class__.__name__}"
+        )
+
+    # pylint: disable-next=arguments-differ
+    def fill_flattened(  # type: ignore[override]
+        self: Self,
+        obj: Any = None,
+        *,
+        weight: Any | None = None,
+        sample: Any | None = None,
+        threads: int | None = None,
+        **kwargs: Any,
+    ) -> Self:
+        axis_names = {ax.name for ax in self.axes}
+
+        non_user_kwargs = {}
+        if weight is not None:
+            non_user_kwargs["weight"] = weight
+        if sample is not None:
+            non_user_kwargs["sample"] = sample
+
+        # Single arguments are either arrays for single-dimensional histograms, or
+        # structures for multi-dimensional hists that must first be unpacked
+        if obj is not None:
+            if kwargs:
+                raise TypeError(
+                    "Only explicit keyword arguments, or a single structured object is supported by "
+                    "`fill_flattened`, but not both."
+                )
+
+            destructured = interop.destructure(obj)
+            # Try to unpack the array, if it's valid to do so, i.e. is the Awkward Array a record array?
+            if destructured is None:
+                raise TypeError(
+                    f"Only fill by names are supported for {self.__class__.__name__}. A single object was given to "
+                    "`fill_flattened`, but it could not be destructed into name-array pairs."
+                )
+
+            # Result must be broadcast, so unpack and rebuild
+            broadcast = interop.broadcast_and_flatten(
+                (*destructured.values(), *non_user_kwargs.values())
+            )
+            # Partition into user and non-user args
+            user_kwargs_broadcast = {
+                k: v
+                for k, v in zip(destructured, broadcast[: len(destructured)])
+                if k in axis_names
+            }
+            non_user_kwargs_broadcast = dict(
+                zip(non_user_kwargs, broadcast[len(destructured) :])
+            )
+        # Multiple args: broadcast and flatten!
+        else:
+            inputs = (*kwargs.values(), *non_user_kwargs)
+            broadcast = interop.broadcast_and_flatten(inputs)
+            user_kwargs_broadcast = dict(zip(kwargs, broadcast[: len(kwargs)]))
+            non_user_kwargs_broadcast = dict(
+                zip(non_user_kwargs, broadcast[len(kwargs) :])
+            )
+        return self.fill(
+            **user_kwargs_broadcast,
+            threads=threads,
+            **non_user_kwargs_broadcast,
         )
 
     def __getitem__(  # type: ignore[override]
