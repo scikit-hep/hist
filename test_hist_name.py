890a36b740ec0956e3e8fbdc39fa0dcf658b7ef0
Harry Patcher <harry-patcher@users.noreply.github.com>
2025-05-21 18:26:29 +0000

    Fix name attribute in Hist constructor

---
+++ test_hist_name.py
@@ -0,0 +1,12 @@
+import hist
+import boost_histogram as bh
+
+# Test case 1: Creating from axes with name (should work)
+a = hist.axis.Regular(10, 0, 10, name="r")
+h1 = hist.Hist(a, name="hello")
+print("Test 1 - Name from axes:", h1.name)  # Should print "hello"
+
+# Test case 2: Creating from bh.Histogram with name (was broken)
+bh_hist = bh.Histogram(a)
+h2 = hist.Hist(bh_hist, name="world")
+print("Test 2 - Name from bh.Histogram:", h2.name)  # Should print "world"
