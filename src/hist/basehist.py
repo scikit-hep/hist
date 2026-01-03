9bed9c7e298d1739f11ead41dcf4540ab0896d1f
Rachit931 <symbolused@gmail.com>
2026-01-04 01:17:23 +0530

    imporve error message for invalid weighted fill

--- src/hist/basehist.py
+++ src/hist/basehist.py
@@ -305,6 +305,15 @@ class BaseHist(bh.Histogram, metaclass=MetaConstructor, family=hist):
 
         data = (data_dict[i] for i in range(len(args), self.ndim))
 
+        # ----START. Clearer error for invalid weighted fill : 
+        if weight is not None and args : 
+            if hasattr(weight, "__len__") and hasattr(args[0], "__len__") : 
+                if len(weight) != len(args[0]) : 
+                    raise ValueError ( 
+                        "Weight array must match the legnth of the input data"
+                    )
+        # ----END.
+
         return super().fill(*args, *data, weight=weight, sample=sample, threads=threads)
 
     def fill_flattened(
