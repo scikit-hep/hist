d6efce5ed0a23638e2ea6aa027abbd41051a8dff
Rachit931 <symbolused@gmail.com>
2026-01-06 17:18:34 +0530

    Fix unused variable in plot_ratio regression test

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -867,6 +867,8 @@ def test_plot_ratio_misalignment():
     def record_x(x, *a, **k):
         captured["x"] = np.asarray(x)
         return []
+    
+    _, ax = plt.subplots() 
 
     fig, ax = plt.subplots()
 
