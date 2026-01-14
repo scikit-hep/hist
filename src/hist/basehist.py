1b88f20eea8cb66dbe0e7e4b7580d6dcdfd7d5f3
Iason Krommydas <iason.krom@gmail.com>
2026-01-14 21:15:42 +0100

    fix issue and add tests

--- src/hist/basehist.py
+++ src/hist/basehist.py
@@ -305,12 +305,19 @@ class BaseHist(bh.Histogram, metaclass=MetaConstructor, family=hist):
 
         data = (data_dict[i] for i in range(len(args), self.ndim))
 
+        def can_check_length(x: Any) -> bool:
+            is_dak_like = all(
+                hasattr(x, attr)
+                for attr in ["__dask_graph__", "layout", "fields", "known_divisions"]
+            )
+            return x.known_divisions if is_dak_like else hasattr(x, "__len__")
+
         # Quick check for a common mistake with a weighted fill
         if (
             weight is not None
             and args
-            and hasattr(weight, "__len__")
-            and hasattr(args[0], "__len__")
+            and can_check_length(weight)
+            and can_check_length(args[0])
             and len(weight) != len(args[0])
         ):
             raise ValueError(
--- tests/test_dask.py
+++ tests/test_dask.py
@@ -5,8 +5,50 @@ import pytest
 
 import hist
 
+ak = pytest.importorskip("awkward")
 da = pytest.importorskip("dask.array")
 dah = pytest.importorskip("hist.dask")
+dak = pytest.importorskip("dask_awkward")
+
+
+@pytest.mark.parametrize("unknown_divisions", [False, True])
+@pytest.mark.parametrize("npartitions", [1, 2, 5])
+@pytest.mark.parametrize("use_weights", [True, False])
+def test_simple_1D_dask_awkward(
+    unnamed_dask_hist, use_weights, npartitions, unknown_divisions
+):
+    x = ak.Array(np.random.standard_normal(size=1000))
+    x = dak.from_awkward(x, npartitions=npartitions)
+    if unknown_divisions:
+        x.clear_divisions()
+    if use_weights:
+        weights = ak.Array(np.random.uniform(0.5, 0.75, size=1000))
+        weights = dak.from_awkward(weights, npartitions=npartitions)
+        if unknown_divisions:
+            weights.clear_divisions()
+        storage = hist.storage.Weight()
+    else:
+        weights = None
+        storage = hist.storage.Double()
+
+    h = unnamed_dask_hist(
+        hist.axis.Regular(10, -4.0, 4.0, name="x"),
+        storage=storage,
+    )
+    h.fill(x, weight=weights)
+    h = h.compute()
+
+    control = h.__class__(*h.axes, storage=h.storage_type())
+    xc = x.compute()
+    if use_weights:
+        wc = weights.compute()
+        control.fill(xc, weight=wc)
+    else:
+        control.fill(xc)
+
+    assert np.allclose(h.counts(), control.counts())
+    if use_weights:
+        assert np.allclose(h.variances(), control.variances())
 
 
 @pytest.mark.parametrize("use_weights", [True, False])
