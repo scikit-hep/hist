fix: protect variance calc as pull
Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

src/hist/plot.py
@@ -672,6 +672,7 @@ def _plot_ratiolike(
 
     # Computation and Fit
     hist_values = self.values()
+    hist_values_uncert: np.typing.NDArray[Any] | None = None
 
     main_ax_artists: MainAxisArtists  # Type now due to control flow
     if callable(other) or isinstance(other, str):
@@ -720,10 +721,8 @@ def _plot_ratiolike(
 
         # Compute histogram uncertainties for pull plots
         variances = self.variances()
-        if variances is None:
-            msg = "Cannot compute from a variance-less histogram, try a Weight storage"
-            raise RuntimeError(msg)
-        hist_values_uncert = np.sqrt(variances)
+        if variances is not None:
+            hist_values_uncert = np.sqrt(variances)
 
     subplot_ax_artists: RatiolikeArtists  # Type now due to control flow
     # Compute ratios: containing no INF values
@@ -741,6 +740,9 @@ def _plot_ratiolike(
             )
 
         elif view == "pull":
+            if hist_values_uncert is None:
+                msg = "Cannot compute from a variance-less histogram, try a Weight storage"
+                raise RuntimeError(msg)
             pulls: np.typing.NDArray[Any] = (
                 hist_values - compare_values
             ) / hist_values_uncert